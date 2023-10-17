0e7d89aba4cb223850bd0a32eb522223e2a4f600
AdrianBaM <110060063+AdrianBaM@users.noreply.github.com>
2023-10-16 18:35:45 -0600

    Creacion de modelos

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Dispositivo(models.Model):
+    equipo = models.CharField(max_length=30, verbose_name='Equipo')
+
+    def nombreDispositivo(self):
+        return "{}".format(self.equipo)
+    
+    def __str__(self):
+        return self.equipo
+
+class Task(models.Model):
+    numeroPaciente = models.CharField(max_length=20, verbose_name='Número de Paciente')
+    Dispositivo = models.ForeignKey(Dispositivo, null=True, on_delete=models.CASCADE)
+    tiempo = models.TimeField(verbose_name='Tiempo realizado')
+    fecha =  models.DateField(verbose_name='Fecha')
+
+    def __str__(self):
+        return self.numeroPaciente
