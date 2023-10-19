5b01e3ed627c5fcac6974abe3ac47762abf5e391
adrian1639 <110060063+AdrianBaM@users.noreply.github.com>
2023-10-19 15:42:34 -0600

    Cambios en la UI y conexion paralela del MQTT

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ import image3 from '../assets/imagen3.png';
 import { createTask } from '../api/tasks.api';
 
 export function Home() {
-  const [pulsoCardiaco, setPulsoCardiaco] = useState(null);
+  const [pulsosCardiacos, setPulsosCardiacos] = useState([null, null, null]);
   const [client, setClient] = useState(null);
   useEffect(() => {
     const client = new Client('ws://192.168.10.134:9001/', 'dash-client');
@@ -21,18 +21,29 @@ export function Home() {
     };
 
     client.onMessageArrived = (message) => {
-        console.log(`Mensaje recibido en el tema ${message.destinationName}: ${message.payloadString}`);
-        
-        // Extrae la cadena de pulso directamente
-        const pulsoPart = message.payloadString.split('Pulso: ')[1];
+      console.log(`Mensaje recibido en el tema ${message.destinationName}: ${message.payloadString}`);
       
-        setPulsoCardiaco(pulsoPart);
+      // Extrae la cadena de pulso directamente
+      const pulsoPart = message.payloadString.split('Pulso: ')[1];
+      
+      if (message.destinationName === 'g1/pulso') {
+        // Asocia el pulso con la tarjeta 1
+        setPulsosCardiacos([pulsoPart, pulsosCardiacos[1], pulsosCardiacos[2]]);
+      } else if (message.destinationName === 'g2/pulso') {
+        // Asocia el pulso con la tarjeta 2
+        setPulsosCardiacos([pulsosCardiacos[0], pulsoPart, pulsosCardiacos[2]]);
+      } else if (message.destinationName === 'g3/pulso') {
+        // Asocia el pulso con la tarjeta 3
+        setPulsosCardiacos([pulsosCardiacos[0], pulsosCardiacos[1], pulsoPart]);
+      }
     };
     
     client.connect({
       onSuccess: () => {
         console.log('Conexión al broker MQTT exitosa');
         client.subscribe('g1/pulso');
+        client.subscribe('g2/pulso');
+        client.subscribe('g3/pulso');
       },
       useSSL: false,
       userName: 'esdras',
@@ -260,63 +271,64 @@ export function Home() {
   };
 
   return (
-    <div className="container">
-      {cards.map((card, index) => (
-        <div key={index} className={`card ${card.state === 2 ? 'estado-2' : ''} ${card.state === 1 ? 'estado-1' : ''}`}>
-          <div className="image">
-            <img src={images[index]} alt={`Imagen ${index + 1}`} />
-          </div>
-          <div className="center">
-            {card.state === 0 && (
-              <button className="iniciar" onClick={() => iniciarProceso(index)}>
-                <FontAwesomeIcon icon={faPlay} /> Iniciar
-              </button>
-            )}
-            {card.state === 1 && (
-              <div className="form">
-                <div className="input-column">
-                  <input
-                    type="text"
-                    placeholder="Número de Paciente"
-                    value={card.numeroPaciente}
-                    onChange={(e) => {
-                      const updatedCards = [...cards];
-                      updatedCards[index].numeroPaciente = e.target.value;
-                      setCards(updatedCards);
-                    }}
-                  />
-                  <br /> <br />
-                  <input
-                    type="text"
-                    placeholder="Tiempo"
-                    value={card.tiempo}
-                    onChange={(e) => {
-                      const updatedCards = [...cards];
-                      updatedCards[index].tiempo = e.target.value;
-                      setCards(updatedCards);
-                    }}
-                  />
-                </div>
-                <div className="buttons-column">
-                  <button className="iniciar" onClick={() => iniciarTerapia(index)}>
-                    <FontAwesomeIcon icon={faPlay} /> Empezar
-                  </button>
-                  <button className="cancelar" onClick={() => cancelarProceso(index)}>
-                    <FontAwesomeIcon icon={faBan} /> Cancelar
-                  </button>
-                </div>
-              </div>
-            )}
-            {card.state === 2 && (
-              <>
-                <div className="numero-paciente">Código de paciente: {card.numeroPaciente}</div>
+  <div className="container">
+    {cards.map((card, index) => (
+      <div key={index} className={`card ${card.state === 2 ? 'estado-2' : ''} ${card.state === 1 ? 'estado-1' : ''}`}>
+        <div className="image">
+          <img src={images[index]} alt={`Imagen ${index + 1}`} />
+        </div>
+        <div className="center">
+          {card.state === 0 && (
+            <button className="iniciar" onClick={() => iniciarProceso(index)}>
+              <FontAwesomeIcon icon={faPlay} /> Iniciar
+            </button>
+          )}
+          {card.state === 1 && (
+            <div className="form">
+              <div className="input-column">
+                <input
+                  type="text"
+                  placeholder="Número de Paciente"
+                  value={card.numeroPaciente}
+                  onChange={(e) => {
+                    const updatedCards = [...cards];
+                    updatedCards[index].numeroPaciente = e.target.value;
+                    setCards(updatedCards);
+                  }}
+                />
                 <br /> <br />
-                <button className="parar" onClick={() => pararTerapia(index)}>
-                  <FontAwesomeIcon icon={faStop} /> Parar
+                <input
+                  type="text"
+                  placeholder="Tiempo"
+                  value={card.tiempo}
+                  onChange={(e) => {
+                    const updatedCards = [...cards];
+                    updatedCards[index].tiempo = e.target.value;
+                    setCards(updatedCards);
+                  }}
+                />
+              </div>
+              <div className="buttons-column">
+                <button className="iniciar" onClick={() => iniciarTerapia(index)}>
+                  <FontAwesomeIcon icon={faPlay} /> Empezar
                 </button>
-              </>
-            )}
-          </div>
+                <button className="cancelar" onClick={() => cancelarProceso(index)}>
+                  <FontAwesomeIcon icon={faBan} /> Cancelar
+                </button>
+              </div>
+            </div>
+          )}
+          {card.state === 2 && (
+            <>
+              <div className="numero-paciente">Código de paciente: {card.numeroPaciente}</div>
+              <br /> <br />
+              <button className="parar" onClick={() => pararTerapia(index)}>
+                <FontAwesomeIcon icon={faStop} /> Parar
+              </button>
+            </>
+          )}
+        </div>
+        {index < 2 && (
           <div className='form2'>
             <div className="slider-container">
               <div className={`slider-track state-${state}`} onClick={handleSliderChange}>
@@ -337,15 +349,16 @@ export function Home() {
               </button>
             </div>
           </div>
-          <div className="timer">
-            <FontAwesomeIcon icon={faStopwatch} /> {Math.floor(card.tiempoRestante / 3600)}:{Math.floor((card.tiempoRestante % 3600) / 60)}:{card.tiempoRestante % 60 < 10 ? '0' : ''}{card.tiempoRestante % 60}
-          </div>
-          <div className="pulso-cardiaco">
-            <FontAwesomeIcon icon={faHeart} beat /> {pulsoCardiaco !== null ? pulsoCardiaco : 'Cargando...'}
-          </div>
+        )}
+        <div className="timer">
+          <FontAwesomeIcon icon={faStopwatch} /> {Math.floor(card.tiempoRestante / 3600)}:{Math.floor((card.tiempoRestante % 3600) / 60)}:{card.tiempoRestante % 60 < 10 ? '0' : ''}{card.tiempoRestante % 60}
         </div>
-      ))}
-    </div>
+        <div className="pulso-cardiaco">
+          <FontAwesomeIcon icon={faHeart} beat /> {pulsosCardiacos[index] !== null ? pulsosCardiacos[index] : 'Cargando...'}
+        </div>
+      </div>
+    ))}
+  </div>
   );
 }
 
